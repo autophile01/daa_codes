90b4916d97c88278d75c78e44047fdf3d21b927a
Aishwarya Motghare <91952403+autophile01@users.noreply.github.com>
2023-11-07 13:09:51 +0530

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -14,3 +14,45 @@ if __name__ == "__main__":
     sorted_list = quicksort(input_list)
     
     print("Sorted list: ", sorted_list)
+
+
+
+MERGE SORT
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Find the middle of the array
+        left_half = arr[:mid]  # Divide the array into two halves
+        right_half = arr[mid:]
+
+        merge_sort(left_half)  # Recursively sort the left half
+        merge_sort(right_half)  # Recursively sort the right half
+
+        i = j = k = 0
+
+        # Merge the two halves back together
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Check if any element was left
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+if __name__ == "__main__":
+    user_input = input("Enter a list of numbers separated by spaces: ")
+    input_list = [int(x) for x in user_input.split()]
+
+    merge_sort(input_list)
+    print("Sorted list: ", input_list)
